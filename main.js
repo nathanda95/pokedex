ef6132b2b4137ea7e51873f46826c34020bce20e
nathanda95 <nathan.daligault@diduenjoy.com>
2024-09-16 15:22:52 +0200

    [feat]: add clickable link for evo card

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const url = "https://tyradex.tech/api/v1/pokemon";
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("Ready !!!!!")
+  //console.log("Ready !!!!!")
   const input = document.getElementById('myInput');
   const name = document.getElementById('nom');
   const image = document.getElementById('imagePokemon');
@@ -76,7 +76,123 @@ document.addEventListener('DOMContentLoaded', () => {
             poids.innerText = response.data.weight;
             taille.innerText = response.data.height;
           } else {
-            alert('No image found for this Pokémon.');
+            if (encodeURIComponent(value) === 'zack')
+            {
+              image.hidden = false;
+              image.src = 'images/chevalzack.png';
+              return;
+            }
+            else
+            {
+              alert('No image found for this Pokémon.');
+            }
+          }
+          if (response.data.evolution.next && response.data.evolution.next.length === 1)
+          {
+            var url3 = `${url}/${response.data.evolution.next[0].name}`
+            console.log(url3)
+            axios.get(url3).then(response2 => {
+              console.log(response2);
+              imageEvo1.src = response2.data.sprites.regular;
+            })
+            textEvo1.innerText = response.data.evolution.next[0].name
+            divEvo1.removeAttribute("hidden")
+            divEvo2.setAttribute("hidden", true);
+          }
+          else if (response.data.evolution.next && response.data.evolution.next.length === 2)
+          {
+            var url3 = `${url}/${response.data.evolution.next[0].name}`
+            axios.get(url3).then(response2 => {
+              imageEvo1.src = response2.data.sprites.regular;
+            })
+            textEvo1.innerText = response.data.evolution.next[0].name;
+            divEvo1.removeAttribute("hidden");
+            var url4 = `${url}/${response.data.evolution.next[1].name}`
+            axios.get(url4).then(response2 => {
+              imageEvo2.src = response2.data.sprites.regular;
+            })
+            textEvo2.innerText = response.data.evolution.next[1].name
+            divEvo2.removeAttribute("hidden");
+          }
+          else
+          {
+            divEvo1.setAttribute("hidden", true);
+            divEvo2.setAttribute("hidden", true);
+          }
+        })
+        .catch(error => {
+          if (encodeURIComponent(value) === 'zack')
+          {
+            image.hidden = false;
+            image.src = 'images/chevalzack.png';
+            return;
+          }
+          console.error('There was an error with the request!', error);
+          alert('Error with the request: ' + error.message);
+        });
+    }
+  });
+
+  gen.addEventListener('click', (event) => {
+    principal.removeAttribute('hidden');
+    evolution.setAttribute('hidden', 'true');
+  });
+
+  evolve.addEventListener('click', (event) => {
+    console.log(res)
+    principal.setAttribute('hidden', 'true');
+    evolution.removeAttribute('hidden');
+  })
+
+  other.addEventListener('click', (event) => {
+    principal.setAttribute('hidden', 'true');
+    evolution.setAttribute('hidden', 'true');
+  })
+
+  divEvo1.addEventListener('click', (event) => {
+      const url2 = `${url}/${encodeURIComponent(res.data.evolution.next[0].name)}`; // Encode les caractères spéciaux dans l'URL
+
+      //console.log('Requesting URL:', url2); // Affiche la valeur dans la console
+      image.src='images/cergle.gif'
+      axios.get(url2)
+        .then(response => {
+          console.log('API response:', response.data); // Inspectez la réponse de l'API
+          res = response
+          if (response.data && response.data.sprites && response.data.sprites.regular) {
+            name.innerText = response.data.name.fr;
+            image.hidden = false;
+            image.src = response.data.sprites.regular;
+            nPokedex.innerText = response.data.pokedex_id;
+            if (response.data.types.length === 2)
+            {
+              titreType.innerText = "Types: "
+              trait.hidden = false;
+              type1.src = response.data.types[0].image;
+              type2.src = response.data.types[1].image;
+              type2.hidden = false;
+              type1.hidden = false;
+            }
+            else
+            {
+              titreType.innerText = "Type: "
+              trait.hidden = true;
+              type1.src = response.data.types[0].image;
+              type2.hidden = true;
+              type1.hidden = false;
+            }
+            poids.innerText = response.data.weight;
+            taille.innerText = response.data.height;
+          } else {
+            if (encodeURIComponent(value) === 'zack')
+            {
+              image.hidden = false;
+              image.src = 'images/chevalzack.png';
+              return;
+            }
+            else
+            {
+              alert('No image found for this Pokémon.');
+            }
           }
           if (response.data.evolution.next && response.data.evolution.next.length === 1)
           {
@@ -115,26 +231,104 @@ document.addEventListener('DOMContentLoaded', () => {
           {
             image.hidden = false;
             image.src = 'images/chevalzack.png';
+            return;
           }
           console.error('There was an error with the request!', error);
           alert('Error with the request: ' + error.message);
         });
-    }
+        principal.removeAttribute('hidden');
+        evolution.setAttribute('hidden', 'true');
   });
 
-  gen.addEventListener('click', (event) => {
-    principal.removeAttribute('hidden');
-    evolution.setAttribute('hidden', 'true');
-  });
+  divEvo2.addEventListener('click', (event) => {
+    const url2 = `${url}/${encodeURIComponent(res.data.evolution.next[1].name)}`; // Encode les caractères spéciaux dans l'URL
 
-  evolve.addEventListener('click', (event) => {
-    console.log(res)
-    principal.setAttribute('hidden', 'true');
-    evolution.removeAttribute('hidden');
-  })
+    //console.log('Requesting URL:', url2); // Affiche la valeur dans la console
+    image.src='images/cergle.gif'
+    axios.get(url2)
+      .then(response => {
+        console.log('API response:', response.data); // Inspectez la réponse de l'API
+        res = response
+        if (response.data && response.data.sprites && response.data.sprites.regular) {
+          name.innerText = response.data.name.fr;
+          image.hidden = false;
+          image.src = response.data.sprites.regular;
+          nPokedex.innerText = response.data.pokedex_id;
+          if (response.data.types.length === 2)
+          {
+            titreType.innerText = "Types: "
+            trait.hidden = false;
+            type1.src = response.data.types[0].image;
+            type2.src = response.data.types[1].image;
+            type2.hidden = false;
+            type1.hidden = false;
+          }
+          else
+          {
+            titreType.innerText = "Type: "
+            trait.hidden = true;
+            type1.src = response.data.types[0].image;
+            type2.hidden = true;
+            type1.hidden = false;
+          }
+          poids.innerText = response.data.weight;
+          taille.innerText = response.data.height;
+        } else {
+          if (encodeURIComponent(value) === 'zack')
+          {
+            image.hidden = false;
+            image.src = 'images/chevalzack.png';
+            return;
+          }
+          else
+          {
+            alert('No image found for this Pokémon.');
+          }
+        }
+        if (response.data.evolution.next && response.data.evolution.next.length === 1)
+        {
+          var url3 = `${url}/${response.data.evolution.next[0].name}`
+          console.log(url3)
+          axios.get(url3).then(response2 => {
+            console.log(response2);
+            imageEvo1.src = response2.data.sprites.regular;
+          })
+          textEvo1.innerText = response.data.evolution.next[0].name
+          divEvo1.removeAttribute("hidden")
+        }
+        else if (response.data.evolution.next && response.data.evolution.next.length === 2)
+        {
+          var url3 = `${url}/${response.data.evolution.next[0].name}`
+          axios.get(url3).then(response2 => {
+            imageEvo1.src = response2.data.sprites.regular;
+          })
+          textEvo1.innerText = response.data.evolution.next[0].name;
+          divEvo1.removeAttribute("hidden");
+          var url4 = `${url}/${response.data.evolution.next[1].name}`
+          axios.get(url4).then(response2 => {
+            imageEvo2.src = response2.data.sprites.regular;
+          })
+          textEvo2.innerText = response.data.evolution.next[1].name
+          divEvo2.removeAttribute("hidden");
+        }
+        else
+        {
+          divEvo1.setAttribute("hidden", true);
+          divEvo2.setAttribute("hidden", true);
+        }
+      })
+      .catch(error => {
+        if (encodeURIComponent(value) === 'zack')
+        {
+          image.hidden = false;
+          image.src = 'images/chevalzack.png';
+          return;
+        }
+        console.error('There was an error with the request!', error);
+        alert('Error with the request: ' + error.message);
+      });
+      principal.removeAttribute('hidden');
+      evolution.setAttribute('hidden', 'true');
+});
 
-  other.addEventListener('click', (event) => {
-    principal.setAttribute('hidden', 'true');
-    evolution.setAttribute('hidden', 'true');
-  })
 });
